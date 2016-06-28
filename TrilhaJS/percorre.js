c8c1690d82fe2a29739e809cc4b5b44af768d514
mateusteixeira <mateusteixeira_@hotmail.com>
2016-06-27 23:48:42 -0300

    Aula 5 terminada

---
+++ TrilhaJS/percorre.js
@@ -0,0 +1,10 @@
+//arquivo percorre.js                        
+
+function percorreArray(trsPacientes, comportamento) {                        
+
+    for(var posicaoAtual = 0; posicaoAtual <= trsPacientes.length - 1; posicaoAtual++){                    
+
+        var pacienteTrAtual = trsPacientes[posicaoAtual];                
+        comportamento(pacienteTrAtual);                
+    }                    
+}
