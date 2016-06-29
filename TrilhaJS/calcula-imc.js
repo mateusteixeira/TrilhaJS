dec1a48fd1a3e34d6f316b97dbdd4378a8a01c54
mateusteixeira <mateusteixeira_@hotmail.com>
2016-06-28 21:34:25 -0300

    JavaScript I terminado

--- TrilhaJS/calcula-imc.js
+++ TrilhaJS/calcula-imc.js
@@ -1,39 +1,45 @@
 //arquivo calcula-imc.js
 
-var trsPacientes = document.getElementsByClassName("paciente");
 
-percorreArray(trsPacientes, imprimeEModificaTdDeImc);
+var botao = document.getElementById("calcula-imcs");
 
-function imprimeEModificaTdDeImc(pacienteTr){
+botao.addEventListener("click",function (){
 
-    var pacienteAtual = montaPaciente(pacienteTr);    
-    var imc = pacienteAtual.pegaImc();
+    var trsPacientes = document.getElementsByClassName("paciente");
 
-    var tdImc = pacienteTr.getElementsByClassName("info-imc")[0]; 
-    tdImc.textContent = imc;
+    percorreArray(trsPacientes, imprimeEModificaTdDeImc);
 
-    console.log(imc);
-}
+    function imprimeEModificaTdDeImc(pacienteTr){
 
-function montaPaciente(pacienteTr){
-    var tdNome = pacienteTr.getElementsByClassName("info-nome")[0]; 
-    var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0]; 
-    var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+        var pacienteAtual = montaPaciente(pacienteTr);    
+        var imc = pacienteAtual.pegaImc();
 
-    var paciente = {
-        nome : tdNome.textContent, 
-        peso : tdPeso.textContent, 
-        altura : tdAltura.textContent,
-        pegaImc: function() {
+        var tdImc = pacienteTr.getElementsByClassName("info-imc")[0]; 
+        tdImc.textContent = imc;
 
-            if(this.altura != 0){
-                var imc = this.peso / (this.altura * this.altura);
-                return imc;
-            } else{
+        console.log(imc);
+    }
 
-                console("Não posso dividir por zero!");
+    function montaPaciente(pacienteTr){
+        var tdNome = pacienteTr.getElementsByClassName("info-nome")[0]; 
+        var tdPeso = pacienteTr.getElementsByClassName("info-peso")[0]; 
+        var tdAltura = pacienteTr.getElementsByClassName("info-altura")[0];
+
+        var paciente = {
+            nome : tdNome.textContent, 
+            peso : tdPeso.textContent, 
+            altura : tdAltura.textContent,
+            pegaImc: function() {
+
+                if(this.altura != 0){
+                    var imc = this.peso / (this.altura * this.altura);
+                    return imc;
+                } else{
+
+                    console("Não posso dividir por zero!");
+                }
             }
         }
+        return paciente;
     }
-    return paciente;
-}
+});
